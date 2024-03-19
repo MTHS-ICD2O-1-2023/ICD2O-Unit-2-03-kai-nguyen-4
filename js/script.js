4e75d0d1b46fc7b8c33b774a27c1e30e0537253b
Kai Nguyen <159185076+kai-nguyen-4@users.noreply.github.com>
2024-03-19 18:32:24 +0000

    created a form site

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Kai Nguyen All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kai Nguyen
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+function enterClicked() {
+  const firstName = document.getElementById("first-name").value
+  const userAge = parseInt(document.getElementById("age-entered").value)
+  document.getElementById("answer").innerHTML = "Your info is: " + firstName + ", age " + userAge + "."
+}
